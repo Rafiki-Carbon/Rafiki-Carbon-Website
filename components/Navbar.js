4dbc39dd6dad4af7ab4ec5d5ee46515f9b290a0d
monginadiana <dianahmongina2@gmail.com>
2023-08-30 19:28:03 +0300

    formating

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,83 +1,38 @@
-import Link from 'next/link';
-import Image from 'next/image';
-
+import Link from "next/link";
+import Image from "next/image";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ri";
 
 export default function Navbar() {
-   
-   
   return (
-    <div
-      
-      className=" w-full "
-    >
+    <div className=" w-full ">
       <div className="m-auto flex justify-between items-center  text-white">
-       <div className="">
-       <Link href="/" className="font-bold text-4xl text-[#000000] ">
-        <Image
-          src={"./rafiki-logo.svg"}
-          width={220}
-          height={60}
-          alt=""
-          priority={true}
-        />  
-        </Link>
-       </div>
-        <div className=''>
-        <ul  className="hidden sm:flex text-[#000000]">
-          <li className="p-4">
-            <Link href="/">Approach</Link>
-          </li>
-          <li className="p-4">
-            <Link href="/#gallery">Team</Link>
-          </li>
-          <li className="p-4">
-            <Link href="/work">FAQs</Link>
-          </li>
-          <li className="px-10 py-4 border-2 rounded-[20px] border-[#008080]">
-            <Link href="/contact">Get Started</Link>
-          </li>
-        </ul>
-
-        </div>
-        {/* Mobile button */}
-        {/* <div className="sm:hidden block z-10">
-          {nav ? (
-            <AiOutlineClose
-              className="text-4xl"
-              onClick={toggleNav}
-              style={{ color: textColor }}
+        <div className="">
+          <Link href="/" className="font-bold text-4xl text-[#000000] ">
+            <Image
+              src={"./rafiki-logo.svg"}
+              width={220}
+              height={60}
+              alt=""
+              priority={true}
             />
-          ) : (
-            <AiOutlineMenu
-              className="text-4xl"
-              onClick={toggleNav}
-              style={{ color: textColor }}
-            />
-          )}
+          </Link>
         </div>
-        {/* Mobile menu */}
-        {/* <div
-          className={
-            nav
-              ? 'sm:hidden absolute top-0 right-0 left-0 bottom-0 flex justify-center items-center  w-full h-screen bg-black text-white duration-300 ease-in-out'
-              : 'sm:hidden absolute top-0 right-0 left-[-100%] bottom-0 flex justify-center items-center w-full h-screen bg-black text-white duration-300 ease-in-out'
-          }
-        >
-          <ul onClick={toggleNav}>
-            <li className="p-4 text-4xl hover:text-gray-500">
-              <Link href="/">Home</Link>
+        <div className="">
+          <ul className="hidden sm:flex text-[#000000]">
+            <li className="p-4">
+              <Link href="/">Approach</Link>
             </li>
-            <li className="p-4 text-4xl hover:text-gray-500">
-              <Link href="/#gallery">Gallery</Link>
+            <li className="p-4">
+              <Link href="/#team">Team</Link>
             </li>
-            <li className="p-4 text-4xl hover:text-gray-500">
-              <Link href="/work">Work</Link>
+            <li className="p-4">
+              <Link href="/FAQs">FAQs</Link>
             </li>
-            <li className="p-4 text-4xl hover:text-gray-500">
-              <Link href="/contact">Contact</Link>
+            <li className="px-10 py-4 border-2 rounded-[20px] border-[#008080]">
+              <Link href="/getStarted">Get Started</Link>
             </li>
           </ul>
-        </div> */} 
+        </div>
       </div>
     </div>
   );
