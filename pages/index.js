9e44d9a19b5923585e3cc993ed28a7b3e8e06bdd
Salome <mbulambithe@gmail.com>
2023-08-31 19:44:04 +0300

    feat: creates products section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 // Components
 import Home from "@/components/Home";
+import Product from "@/components/Product";
 
 export default function Index() {
-  return <Home />;
+  return <Home />,
+        <Product/>;
 }
