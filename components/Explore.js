b0d407a8070d25896e33bf3227be5a54a88c0ebb
monginadiana <dianahmongina2@gmail.com>
2023-08-31 12:55:41 +0300

    added the getstarted page

---
+++ components/Explore.js
@@ -0,0 +1,16 @@
+export default function Explore() {
+
+    return (
+     <div className=" mx-auto w-[90%] border  justify-center items-center p-4 border-[2] border-[#2F8A8A] mt-4">
+        <div className="flex flex-col justify-center items-center " >
+            <div className="text-center">
+            
+                <h2 className="text-[#808080] mt-2">Unveil a Greener Horizon with Rafiki Carbon Empowering Sustainability and Redefining Impact</h2>
+            </div>
+            <div className="flex  justify-center items-center">
+                <button className="text-[#FFF] rounded-[10px] bg-[#008080] px-16 py-2 mt-2">Get started</button>
+            </div>
+        </div>
+     </div>
+    )
+  }
