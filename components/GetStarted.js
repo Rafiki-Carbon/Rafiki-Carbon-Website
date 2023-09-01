d83aaef4e03b4e2f37a0c92c14389ed4c0e1c5f3
Job Karani <jobkarani084j@gmail.com>
2023-09-01 05:42:35 +0300

    fix: getstarted section

--- components/GetStarted.js
+++ components/GetStarted.js
@@ -1,14 +1,14 @@
 export default function GetStarted() {
 
     return (
-     <div className=" mx-auto w-[90%] border rounded-[24px] justify-center items-center p-4 border-[2] border-[#2F8A8A] mt-4">
+     <div className=" mx-auto w-[90%] border rounded-[24px] justify-center items-center p-4 border-[2] border-[#2F8A8A] my-8">
         <div className="flex flex-col justify-center items-center " >
             <div className="text-center">
-                <h1 className="text-[#000] font-bold mt-2">Get Started Today</h1>
-                <h2 className="text-[#808080] mt-2">Join our marketplace and gain access to multiple carbon solutions features</h2>
+                <h1 className="text-[#000] text-[40px] font-[600] my-2">Get Started Today</h1>
+                <h2 className="text-[#808080] text-[20px] font-[400] my-2">Join our marketplace and gain access to multiple carbon solutions features</h2>
             </div>
-            <div className="flex  justify-center items-center">
-                <button className="text-[#FFF] rounded-[10px] bg-[#008080] px-16 py-2 mt-2">Get started</button>
+            <div className="flex  justify-center items-center my-4">
+                <button className="text-[#FFF] rounded-[10px] bg-[#008080] px-16 py-2 my-2">Get started</button>
             </div>
         </div>
      </div>
