befaadddeb22767d2cbd5a092f5b411466254590
munenebrian <njerubrianmunene@gmail.com>
2023-09-02 12:31:02 +0300

    fix: contact page mobile responsivity

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -2,31 +2,21 @@ import Image from "next/image";
 
 const ContactForm = () => {
   return (
-    <div className="md:grid lg:grid xl:grid grid-cols-2 px-16 xl:py-16">
+    <div className="md:grid lg:grid xl:grid grid-cols-2 contact xl:py-16">
       <div className="flex items-center justify-center">
         <div>
-          <div className="hidden md:block">
-            <Image
-              className="object-cover"
-              src={"/footer-logo.svg"}
-              width={140}
-              height={120}
-              alt=""
-              priority={true}
-            />
-          </div>
-          <div className="text-medium font-medium px-4">
-            Connecting Renewable and ESG-conscious Corporations through
-            Renewable Energy Certificates for a sustainable future.
+          <div className="text-[40px] side font-medium px-4">
+            Connecting <span className="text-[#0072BB]">Renewable</span> and <span className="text-[#0072BB]">ESG-conscious Corporations</span> through
+            <span className="text-[#0072BB]">Renewable Energy Certificates</span> for a sustainable future.
           </div>
         </div>
       </div>
-      <div>
-        <div className="text-xl font-medium pb-5">Become an Early Adopter</div>
+      <div className="form">
+        <div className="text-[24px] head font-medium pb-5">Empower Sustainability. Get Started</div>
         <div className="">
           {/* Input fields */}
           <div className="md:grid grid-cols-2 gap-4">
-            <div className="sm:mt-4">
+            <div className="sm:mt-4 form-grp">
               <div className="text-sm">Full name*</div>
               <input
                 className="outline-none text-sm rounded px-4 py-3 focus:border focus:border-[#0072BB] w-full"
@@ -34,7 +24,7 @@ const ContactForm = () => {
                 placeholder="Jane Doe"
               />
             </div>
-            <div className="sm:mt-4">
+            <div className="sm:mt-4 form-grp">
               <div className="text-sm">Email Address*</div>
               <input
                 className="outline-none text-sm rounded px-4 py-3 focus:border focus:border-[#0072BB] w-full"
@@ -44,7 +34,7 @@ const ContactForm = () => {
             </div>
           </div>
           <div className="xl:grid grid-cols-2 gap-4 xl:mt-4">
-            <div className="sm:mt-4">
+            <div className="sm:mt-4 form-grp">
               <div className="text-sm">Phone Number*</div>
               <input
                 className="outline-none text-sm rounded px-4 py-3 focus:border focus:border-[#0072BB] w-full"
@@ -52,7 +42,7 @@ const ContactForm = () => {
                 placeholder="+254 7** *** ***"
               />
             </div>
-            <div className="sm:mt-4">
+            <div className="sm:mt-4 form-grp">
               <div className="text-sm">Country*</div>
               <input
                 className="outline-none text-sm rounded px-4 py-3 focus:border focus:border-[#0072BB] w-full"
@@ -62,7 +52,7 @@ const ContactForm = () => {
             </div>
           </div>
           <div className="xl:grid grid-cols-2 gap-4 xl:mt-4">
-            <div className="sm:mt-4">
+            <div className="sm:mt-4 form-grp">
               <div className="text-sm">Role*</div>
               <input
                 className="outline-none text-sm rounded px-4 py-3 focus:border focus:border-[#0072BB] w-full"
